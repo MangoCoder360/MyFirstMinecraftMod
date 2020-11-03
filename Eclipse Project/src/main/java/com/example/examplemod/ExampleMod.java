6c739eea461ff13e2df8fd3ca76186211573b092
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-11-02 16:00:56 -0800

    Update ExampleMod.java

--- Eclipse Project/src/main/java/com/example/examplemod/ExampleMod.java
+++ Eclipse Project/src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,6 +1,9 @@
 package com.example.examplemod;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -10,16 +13,22 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String MODID = "sword";
+    public static final String NAME = "Custom Sword";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
+    
+    public static ToolMaterial myToolMaterial;
+    public static Item mySword;
+    
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+        myToolMaterial = EnumHelper.addToolMaterial(NAME, 4, 10, 80.0F, 150.0F, 10);
+        mySword = new CustomSword();
     }
 
     @EventHandler
